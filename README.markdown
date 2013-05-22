483c1313895d0d981107a24d1809f13d85db88b6
mohammed sadiq <sadiqmmm@gmail.com>
2013-05-22 15:29:04 +0530

    removed the old rails3

--- README.markdown
+++
@@ -1,1 +0,0 @@
-# Ruby on rails [Testing this application]
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'ef8f7c9cf79c2c2e210326e0005f8144fd2825b9e859cdac06fb8215bdfc4cce0f3820db922a7f89b78adb727e357fbd65af87f05ec6b640ec0a31b74331915b'
