8dd100f7a15873761cc30699118616051112c6d0
mohammed sadiq <sadiqmmm@gmail.com>
2013-05-22 15:31:41 +0530

    initial commit with upadated Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c5064fec7eec5cd1379fc31758d78581cf1855e26a82aac0d68a63fa8b3c9a0f7d6e96d55c5940a375d9c9d3b1d1943a2e152ba5789dad75b69404168eb2ed36'
